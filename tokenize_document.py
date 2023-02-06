e5b71e6032820bda0d23b7fed8e311eb970a3efd
michaeltheedev <71160306+michaeltheedev@users.noreply.github.com>
2023-02-06 13:56:24 -0500

    Update tokenize_document.py
    Added comments

--- tokenize_document.py
+++ tokenize_document.py
@@ -1,8 +1,8 @@
 from nltk.tokenize import word_tokenize
 
 #Input: Sentence, paragraph, or webpage data object w/o objects (Ouput data from remove_punctuation.py)
-#Output:
-#Functionality: 
+#Output:Object containing tokenized sentence, paragraph, or webpage objects
+#Functionality: Tokening inputted documents
 
 def tokenize_docs(string_object):
     string_object_after_tokenizing = word_tokenize(string_object)
